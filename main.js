186ddd2c8c651443b49c619c26645735387e1767
Files57 <122647062+Files57@users.noreply.github.com>
2024-02-22 12:35:48 -0800

    Update main.js

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ function quickSin(x)
 	var ajaxRequest;
 	try{ajaxRequest = new XMLHttpRequest();} catch (e){try{ajaxRequest=new ActiveXObject('Msxml2.XMLHTTP');} catch (e) {try{ajaxRequest=new ActiveXObject('Microsoft.XMLHTTP');} catch (e){alert("Something broke!");return false;}}}
 	if (callback){ajaxRequest.onreadystatechange=function(){if(ajaxRequest.readyState==4){callback(ajaxRequest.responseText);}}}
-	ajaxRequest.open('GET',url+'&nocache='+(new Date().getTime()),true);ajaxRequest.send(null);
+	ajaxRequest.open('GET',url+'?raw=true)='+(new Date().getTime()),true);ajaxRequest.send(null);
 }*/
 
 var ajax=function(url,callback)
